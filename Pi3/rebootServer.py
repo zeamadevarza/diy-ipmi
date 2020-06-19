384730a9ac705a01244b9edef30e795c2601577e
zeamadevarza <tudor.badea@gmail.com>
2020-06-19 23:25:19 +0300

    changed live

--- Pi3/rebootServer.py
+++ Pi3/rebootServer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
+import sys
 
 GPIO.setmode(GPIO.BCM)
 
--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 VIDSOURCE="/dev/video0"
 AUDIO_OPTS=""
 VIDEO_OPTS="-s 640x480 -c:v libx264 -b:v 800000"
-OUTPUT_HLS="-hls_time 10 -hls_list_size 10 -start_number 1"
-ffmpeg -i "$VIDSOURCE" - y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS mystream.m3u8
+OUTPUT_HLS="-hls_time 10 -hls_list_size 10 -start_number 1 -f hls"
+ffmpeg -i "$VIDSOURCE" -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS /var/www/html/live/mystream.m3u8
